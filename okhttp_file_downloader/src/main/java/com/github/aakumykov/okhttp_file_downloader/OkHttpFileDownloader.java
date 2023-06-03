58336e3fadf293b18111fa3743e36e17a45b5f13
Андрей Кумыков <aakumykov@yandex.ru>
2023-06-03 16:27:57 +0300

    Перенёс функционал OkHttpDownloadingHelper в OkHttpFileDownloader.

--- okhttp_file_downloader/src/main/java/com/github/aakumykov/okhttp_file_downloader/OkHttpFileDownloader.java
+++ okhttp_file_downloader/src/main/java/com/github/aakumykov/okhttp_file_downloader/OkHttpFileDownloader.java
@@ -3,6 +3,7 @@ package com.github.aakumykov.okhttp_file_downloader;
 import com.github.aakumykov.okhttp_file_downloader.exceptions.BadResponseException;
 import com.github.aakumykov.okhttp_file_downloader.exceptions.EmptyBodyException;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.OkHttpClient;
@@ -12,8 +13,30 @@ import okhttp3.ResponseBody;
 
 public class OkHttpFileDownloader implements AutoCloseable {
 
+    private static final String TEMP_PREFIX = "temp_";
+    private static final String TEMP_SUFFIX = "_temp";
+
     private final OkHttpClient mOkHttpClient;
     private final OkHttpFileWriter mOkHttpFileWriter;
+
+
+    public static File downloadFile(String sourceURL)
+            throws IOException, BadResponseException, EmptyBodyException
+    {
+        final File tempFile = File.createTempFile(TEMP_PREFIX, TEMP_SUFFIX);
+        downloadFileTo(sourceURL, tempFile);
+        return tempFile;
+    }
+
+
+    public static void downloadFileTo(String sourceURL, File targetFile)
+            throws IOException, BadResponseException, EmptyBodyException
+    {
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        final OkHttpFileWriter okHttpFileWriter = new OkHttpFileWriter(targetFile);
+
+        new OkHttpFileDownloader(okHttpClient, okHttpFileWriter).download(sourceURL);
+    }
 
 
     public OkHttpFileDownloader(OkHttpClient okHttpClient, OkHttpFileWriter okHttpFileWriter) {
