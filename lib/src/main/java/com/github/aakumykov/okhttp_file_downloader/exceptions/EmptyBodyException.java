27410a803d2a6f75b3bebb0f55cffd7dafd814a2
Андрей Кумыков <aakumykov@yandex.ru>
2023-08-04 22:31:45 +0300

    Удалил модуль lib, подключил библиотеку как внешнюю.

--- lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/exceptions/EmptyBodyException.java
+++
@@ -1,7 +0,0 @@
-package com.github.aakumykov.okhttp_file_downloader.exceptions;
-
-public class EmptyBodyException extends OkHttpFileDownloaderException {
-    public EmptyBodyException() {
-        super("Response body is empty");
-    }
-}
--- lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/exceptions/OkHttpFileDownloaderException.java
+++
@@ -1,7 +0,0 @@
-package com.github.aakumykov.okhttp_file_downloader.exceptions;
-
-public abstract class OkHttpFileDownloaderException extends Exception {
-    public OkHttpFileDownloaderException(String message) {
-        super(message);
-    }
-}
