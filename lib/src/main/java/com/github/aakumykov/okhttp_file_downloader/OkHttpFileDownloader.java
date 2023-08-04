b8a030060f48e15bbdf72e403dd98028c0818629
Андрей Кумыков <aakumykov@yandex.ru>
2023-08-04 22:51:04 +0300

    Метод прерывания закачки + коллбек с информацией о прогрессе и завершении скачивания; v0.0.1

--- lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/OkHttpFileDownloader.java
+++ lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/OkHttpFileDownloader.java
@@ -1,11 +1,15 @@
 package com.github.aakumykov.okhttp_file_downloader;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.github.aakumykov.okhttp_file_downloader.exceptions.BadResponseException;
 import com.github.aakumykov.okhttp_file_downloader.exceptions.EmptyBodyException;
 
 import java.io.File;
 import java.io.IOException;
 
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -14,33 +18,31 @@ import okhttp3.ResponseBody;
 public class OkHttpFileDownloader implements AutoCloseable {
 
     private static final String TAG = OkHttpFileDownloader.class.getSimpleName();
-    private static final String TEMP_PREFIX = TAG+"_";
-    private static final String TEMP_SUFFIX = "_"+TAG;
-
     private final OkHttpClient mOkHttpClient;
     private final OkHttpFileWriter mOkHttpFileWriter;
+    @Nullable private Call mCall;
 
 
-    public static File downloadFile(String sourceURL)
-            throws IOException, BadResponseException, EmptyBodyException
-    {
-        final File tempFile = File.createTempFile(TEMP_PREFIX, TEMP_SUFFIX);
-        downloadFileTo(sourceURL, tempFile);
-        return tempFile;
+    public static OkHttpFileDownloader create(@NonNull File targetFile) throws IOException {
+        return createReal(targetFile, null);
     }
 
+    /*public static OkHttpFileDownloader create(@NonNull File targetFile,
+                                              @NonNull ProgressCallback progressCallback) throws IOException {
+        return createReal(targetFile, progressCallback);
+    }*/
 
-    public static void downloadFileTo(String sourceURL, File targetFile)
-            throws IOException, BadResponseException, EmptyBodyException
-    {
+    private static OkHttpFileDownloader createReal(@NonNull File targetFile,
+                                                   @Nullable ProgressCallback progressCallback) throws IOException {
         final OkHttpClient okHttpClient = new OkHttpClient();
         final OkHttpFileWriter okHttpFileWriter = new OkHttpFileWriter(targetFile);
-
-        new OkHttpFileDownloader(okHttpClient, okHttpFileWriter).download(sourceURL);
+        if (null != progressCallback)
+            okHttpFileWriter.setProgressCallback(progressCallback);
+        return new OkHttpFileDownloader(okHttpClient, okHttpFileWriter);
     }
 
-
-    public OkHttpFileDownloader(OkHttpClient okHttpClient, OkHttpFileWriter okHttpFileWriter) {
+    public OkHttpFileDownloader(OkHttpClient okHttpClient,
+                                OkHttpFileWriter okHttpFileWriter) {
         mOkHttpClient = okHttpClient;
         mOkHttpFileWriter = okHttpFileWriter;
     }
@@ -48,8 +50,9 @@ public class OkHttpFileDownloader implements AutoCloseable {
 
     public void download(String url) throws IOException, BadResponseException, EmptyBodyException {
 
-        final Request request = new Request.Builder().url(url).build();
-        final Response response = mOkHttpClient.newCall(request).execute();
+        mCall = mOkHttpClient.newCall(new Request.Builder().url(url).build());
+
+        final Response response = mCall.execute();
 
         if (!response.isSuccessful())
             throw new BadResponseException(response, url);
@@ -63,8 +66,19 @@ public class OkHttpFileDownloader implements AutoCloseable {
     }
 
 
+    public void cancelDownloading() {
+        if (null != mCall && !mCall.isCanceled())
+            mCall.cancel();
+    }
+
+
     @Override
     public void close() throws Exception {
         mOkHttpFileWriter.close();
+    }
+
+
+    public void setProgressCallback(@NonNull ProgressCallback progressCallback) {
+        mOkHttpFileWriter.setProgressCallback(progressCallback);
     }
 }
--- lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/OkHttpFileWriter.java
+++ lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/OkHttpFileWriter.java
@@ -1,31 +1,56 @@
 package com.github.aakumykov.okhttp_file_downloader;
 
+import androidx.annotation.Nullable;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import okhttp3.ResponseBody;
 import okio.BufferedSink;
+import okio.BufferedSource;
 import okio.Okio;
 
 public class OkHttpFileWriter implements AutoCloseable {
 
     private final BufferedSink mBufferedSink;
+    @Nullable private ProgressCallback mProgressCallback;
 
 
     public OkHttpFileWriter(File outputFile) throws FileNotFoundException {
         mBufferedSink = Okio.buffer(Okio.sink(outputFile));
     }
 
-    public OkHttpFileWriter(FileOutputStream fileOutputStream) {
-        mBufferedSink = Okio.buffer(Okio.sink(fileOutputStream));
+
+    public void setProgressCallback(@Nullable ProgressCallback progressCallback) {
+        this.mProgressCallback = progressCallback;
     }
 
-
     public void write(ResponseBody responseBody) throws IOException {
-        mBufferedSink.writeAll(responseBody.source());
-        mBufferedSink.close();
+
+        try (BufferedSource bufferedSource = responseBody.source()) {
+
+            final byte[] dataBuffer = new byte[1024 * 1024];
+            final long totalBytes = responseBody.contentLength();
+            long loadedBytes = 0;
+            int readedBytes;
+
+            while ((readedBytes = bufferedSource.read(dataBuffer)) != -1) {
+                loadedBytes += readedBytes;
+                mBufferedSink.write(dataBuffer, 0, readedBytes);
+
+                if (null != mProgressCallback)
+                    mProgressCallback.onProgress(
+                            loadedBytes,
+                            totalBytes,
+                            Math.round(1f * loadedBytes / totalBytes * 100.0));
+            }
+
+            mBufferedSink.close();
+
+            if (null != mProgressCallback)
+                mProgressCallback.onComplete();
+        }
     }
 
 
