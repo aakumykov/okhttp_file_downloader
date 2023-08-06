4ce32091e46f78fd7898687bb167a8e4e017d487
Андрей Кумыков <aakumykov@yandex.ru>
2023-08-06 15:04:31 +0300

    Добавил в FileDownloader метод скачивания во временный файл.

--- lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/FileDownloader.java
+++ lib/src/main/java/com/github/aakumykov/okhttp_file_downloader/FileDownloader.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public interface FileDownloader {
     void download(@NonNull final String sourceUrl, @NonNull final File targetFile) throws EmptyBodyException, BadResponseException, IOException;
+    void download(@NonNull final String sourceUrl) throws EmptyBodyException, IOException, BadResponseException;
     void setProgressCallback(ProgressCallback progressCallback);
     void stopDownloading();
 }
